90b59b26e2a60a5b414fef0192878e0504e0d4b0
Tamizharasan R <134991180+Tamizh2R@users.noreply.github.com>
2024-04-16 10:43:54 +0530

    Update and rename java.js to calculator.java

---
+++ calculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SimpleC {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Subtraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+
+        scanner.close();
+    }
+}
